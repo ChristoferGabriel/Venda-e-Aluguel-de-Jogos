5b855f653842ddab65c30fa5929f1458260e6364
Christofer Gabriel <christofergabriel2007@gmail.com>
2026-06-14 23:45:23 -0300

    method convert case

--- src/model/Categoria.java
+++ src/model/Categoria.java
@@ -2,8 +2,26 @@ package model;
 
 public enum Categoria {
     ACAO,
+    AVENTURA,
     RPG,
-    ESPORTE,
-    TERROR,
-    AVENTURA
+    ESPORTE;
+
+    public static Categoria fromString(String texto) {
+
+        texto = texto.toUpperCase()
+                     .replace("Á", "A")
+                     .replace("À", "A")
+                     .replace("Ã", "A")
+                     .replace("Â", "A")
+                     .replace("É", "E")
+                     .replace("Ê", "E")
+                     .replace("Í", "I")
+                     .replace("Ó", "O")
+                     .replace("Ô", "O")
+                     .replace("Õ", "O")
+                     .replace("Ú", "U")
+                     .replace("Ç", "C");
+
+        return Categoria.valueOf(texto);
+    }
 }
